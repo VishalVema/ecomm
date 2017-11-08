92ed07c217035b15eed743e4d57d08b0ab0d6b97
vishal <vishalvermadeveloper@gmail.com>
2017-11-08 21:14:32 +0530

    Cards Design Updated

--- src/components/common/cards.js
+++ src/components/common/cards.js
@@ -8,15 +8,15 @@ class Card extends Component {
     return (
       <div className='col l9'>
         <div className='col l4 margin-bottom-10'>
-          <div className='card small'>
+          <div className='card '>
             <div className='card-image waves-effect waves-block waves-light'>
-              <img className='activator' src='http://materializecss.com/images/office.jpg' />
+              <img className='activator' src='https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1670214/2016/12/5/11480923985371-FabAlley-Women-Black-Solid-A-line-Dress-6691480923985044-1.jpg' />
             </div>
             <div className='card-content'>
-              <span className='card-title activator grey-text text-darken-4'>Card Title
-              <i className='material-icons right'>more_vert</i>
+              <span className='card-title activator grey-text text-darken-4'>Fab Ally
+              <i className='material-icons right tooltipped' data-position="bottom" data-delay="50" data-tooltip="Quick Details">more_vert</i>
               </span>
-              <p><a href='#'>This is a link</a></p>
+
             </div>
             <div className='card-reveal'>
               <span className='card-title grey-text text-darken-4'>Card Title
@@ -24,14 +24,14 @@ class Card extends Component {
               </span>
               <p>Here is some more information about this product that is only revealed once clicked on.</p>
             </div>
-            <div className='card-tabs'>
+            <div className='card-tabs card-shadow'>
               <ul className='tabs tabs-fixed-width'>
                 <li className='tab'><a href='#test7'>Test 1</a></li>
                 <li className='tab'><a className='active' href='#test8'>Test 2</a></li>
                 <li className='tab'><a href='#test9'>Test 3</a></li>
               </ul>
             </div>
-            <div className='card-content grey lighten-4'>
+            <div className='card-content grey lighten-4 card-shadow'>
               <div id='test7'>Test 1</div>
               <div id='test8'>Test 2</div>
               <div id='test9'>Test 3</div>
@@ -39,30 +39,30 @@ class Card extends Component {
           </div>
         </div>
         <div className='col l4 margin-bottom-10'>
-          <div className='card small'>
+          <div className='card '>
             <div className='card-image waves-effect waves-block waves-light'>
-              <img className='activator' src='http://materializecss.com/images/office.jpg' />
+              <img className='activator' src='https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2127401/2017/9/14/11505364906411-RARE-Women-Dresses-7161505364906123-1.jpg' />
             </div>
             <div className='card-content'>
-              <span className='card-title activator grey-text text-darken-4'>Card Title
-              <i className='material-icons right'>more_vert</i>
+              <span className='card-title activator grey-text text-darken-4'>Rare
+              <i className='material-icons right tooltipped' data-position="bottom" data-delay="50" data-tooltip="Quick Details">more_vert</i>
               </span>
-              <p><a href='#'>This is a link</a></p>
+
             </div>
             <div className='card-reveal'>
-              <span className='card-title grey-text text-darken-4'>Card Title
+              <span className='card-title grey-text text-darken-4'>Rare
               <i className='material-icons right'>close</i>
               </span>
               <p>Here is some more information about this product that is only revealed once clicked on.</p>
             </div>
-            <div className='card-tabs'>
+            <div className='card-tabs card-shadow'>
               <ul className='tabs tabs-fixed-width'>
                 <li className='tab'><a href='#test4'>Test 1</a></li>
                 <li className='tab'><a className='active' href='#test5'>Test 2</a></li>
                 <li className='tab'><a href='#test6'>Test 3</a></li>
               </ul>
             </div>
-            <div className='card-content grey lighten-4'>
+            <div className='card-content grey lighten-4 card-shadow'>
               <div id='test4'>Test 1</div>
               <div id='test5'>Test 2</div>
               <div id='test6'>Test 3</div>
@@ -70,46 +70,46 @@ class Card extends Component {
           </div>
         </div>
         <div className='col l4 margin-bottom-10'>
-          <div className='card small'>
+          <div className='card '>
             <div className='card-image waves-effect waves-block waves-light'>
-              <img className='activator' src='http://materializecss.com/images/office.jpg' />
+              <img className='activator' src='https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1644910/2016/12/1/11480590911168-RARE-Women-Black-Solid-Maxi-Dress-3971480590910905-1.jpg' />
             </div>
             <div className='card-content'>
-              <span className='card-title activator grey-text text-darken-4'>Card Title
-              <i className='material-icons right'>more_vert</i>
+              <span className='card-title activator grey-text text-darken-4'>Athena
+              <i className='material-icons right tooltipped' data-position="bottom" data-delay="50" data-tooltip="Quick Details">more_vert</i>
               </span>
-              <p><a href='#'>This is a link</a></p>
+
             </div>
             <div className='card-reveal'>
-              <span className='card-title grey-text text-darken-4'>Card Title
+              <span className='card-title grey-text text-darken-4'>Athena
               <i className='material-icons right'>close</i>
               </span>
               <p>Here is some more information about this product that is only revealed once clicked on.</p>
             </div>
-            <div className='card-tabs'>
-              <ul className='tabs tabs-fixed-width'>
-                <li className='tab'><a href='#test1'>Test 1</a></li>
-                <li className='tab'><a className='active' href='#test2'>Test 2</a></li>
-                <li className='tab'><a href='#test3'>Test 3</a></li>
+            <div className='card-tabs card-shadow'>
+              <ul className='tabs tabs-fixed-width teal  '>
+                <li className='tab yellow-text text-darken-2'><a className='white-text text-darken-2' href='#test1'>Size</a></li>
+                <li className='tab yellow-text text-darken-2'><a className='active white-text text-darken-2' href='#test2'>Buy</a></li>
+                <li className='tab yellow-text text-darken-2'><a className='white-text text-darken-2' href='#test3'>Availabe</a></li>
               </ul>
             </div>
-            <div className='card-content grey lighten-4'>
-              <div id='test1'>Test 1</div>
-              <div id='test2'>Test 2</div>
-              <div id='test3'>Test 3</div>
+            <div className='card-content grey lighten-4 card-shadow'>
+              <div id='test1'>Size</div>
+              <div id='test2'>Buy</div>
+              <div id='test3'>Availabe</div>
             </div>
           </div>
         </div>
         <div className='col l4 margin-bottom-10'>
-          <div className='card small'>
+          <div className='card '>
             <div className='card-image waves-effect waves-block waves-light'>
-              <img className='activator' src='http://materializecss.com/images/office.jpg' />
+              <img className='activator' src='https://assets.myntassets.com/h_1440,q_100,w_1080/v1/image/style/properties/744341/Belle-Fille-White-Printed-Sheath-Dress_1_56f887d3cdb47b7f8af4f24b65ac1e86.jpg' />
             </div>
             <div className='card-content'>
-              <span className='card-title activator grey-text text-darken-4'>Card Title
-              <i className='material-icons right'>more_vert</i>
+              <span className='card-title activator grey-text text-darken-4'>Latin Quarters
+              <i className='material-icons right tooltipped' data-position="bottom" data-delay="50" data-tooltip="Quick Details">more_vert</i>
               </span>
-              <p><a href='#'>This is a link</a></p>
+
             </div>
             <div className='card-reveal'>
               <span className='card-title grey-text text-darken-4'>Card Title
@@ -117,14 +117,14 @@ class Card extends Component {
               </span>
               <p>Here is some more information about this product that is only revealed once clicked on.</p>
             </div>
-            <div className='card-tabs'>
+            <div className='card-tabs card-shadow'>
               <ul className='tabs tabs-fixed-width'>
                 <li className='tab'><a href='#test10'>Test 1</a></li>
                 <li className='tab'><a className='active' href='#test11'>Test 2</a></li>
                 <li className='tab'><a href='#test12'>Test 3</a></li>
               </ul>
             </div>
-            <div className='card-content grey lighten-4'>
+            <div className='card-content grey lighten-4 card-shadow'>
               <div id='test10'>Test 1</div>
               <div id='test11'>Test 2</div>
               <div id='test12'>Test 3</div>
--- src/components/common/header.js
+++ src/components/common/header.js
@@ -13,19 +13,19 @@ class Header extends React.Component {
 
           <nav>
             <div className="nav-wrapper  yellow darken-2">
-              <Link to="/" className="brand-logo">{logoName}</Link>
+              <a href="/" className="brand-logo">{logoName}</a>
               <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
               <ul className="right hide-on-med-and-down">
                 <li><a href="sass.html">Sass</a></li>
                 <li><a href="badges.html">Components</a></li>
                 <li><a href="collapsible.html">Javascript</a></li>
-                <li><Link to="list">Hello</Link></li>
+                <li><a href="list">Hello</a></li>
               </ul>
               <ul className="side-nav" id="mobile-demo">
                 <li><a href="sass.html">Sass</a></li>
                 <li><a href="badges.html">Components</a></li>
                 <li><a href="collapsible.html">Javascript</a></li>
-                <li><Link to="list">Hello</Link></li>
+                <li><a href="list">Hello</a></li>
 
               </ul>
             </div>
